8a13e426b3c3ef6723e30167c70a74a88933c83d
Justin Xia <justinxia9@gmail.com>
2025-07-25 11:30:59 -0400

    Finished working with columns up to review_score

--- sql/data_wrangling.sql
+++ sql/data_wrangling.sql
@@ -212,5 +212,101 @@ SELECT DISTINCT service_fee FROM AirbnbStaging
 SELECT * FROM AirbnbStaging WHERE price IS NULL
 SELECT * FROM AirbnbStaging WHERE service_fee IS NULL
 
--- TODO
--- fill in missing values with average prices
+-- change data types
+ALTER TABLE AirbnbStaging
+ALTER COLUMN price MONEY
+
+ALTER TABLE AirbnbStaging
+ALTER COLUMN service_fee MONEY
+
+-- fill in missing prices with average price of 625
+SELECT AVG(price) FROM AirbnbStaging
+
+UPDATE AirbnbStaging
+SET [price] = 625.00
+WHERE [price] IS NULL
+
+-- fill in missing service fees with average
+SELECT AVG((service_fee/price)*100) FROM AirbnbStaging -- average service fee is 19.9935% of price
+
+UPDATE AirbnbStaging
+SET [service_fee] = ROUND(price*0.199935, 0)
+WHERE [service_fee] IS NULL
+
+-- min_nights
+ALTER TABLE AirbnbStaging
+ALTER COLUMN min_nights INT
+
+-- replace negative values and NULL with 1, meaning there is no minimum stay requirement 
+SELECT DISTINCT min_nights FROM AirbnbStaging
+
+UPDATE AirbnbStaging
+SET [min_nights] = 1
+WHERE [min_nights] < 0 OR [min_nights] IS NULL
+
+-- num_reviews
+ALTER TABLE AirbnbStaging
+ALTER COLUMN num_reviews INT
+
+-- replace NULL values with 0
+SELECT DISTINCT num_reviews FROM AirbnbStaging
+
+UPDATE AirbnbStaging
+SET [num_reviews] = 0
+WHERE [num_reviews] IS NULL
+
+-- last_review date
+SELECT * FROM AirbnbStaging WHERE last_review IS NULL -- 15831 rows without last review date
+
+-- 15796 of those rows have no reviews and should have last_review date left blank
+SELECT * FROM AirbnbStaging WHERE last_review IS NULL
+AND num_reviews = 0
+
+-- 35 of those rows have reviews and reviews_per_month but no last_review date
+SELECT * FROM AirbnbStaging WHERE last_review IS NULL
+AND num_reviews != 0
+
+-- since only 35 rows are missing this date, and it's not as useful for analysis as num_reviews or reviews_per_month, it will also be left blank
+ALTER TABLE AirbnbStaging
+ALTER COLUMN last_review DATE
+
+-- convert reviews_per_month to numeric values
+ALTER TABLE AirbnbStaging
+ALTER COLUMN reviews_per_month FLOAT
+
+-- 15817 rows with no value for reviews_per_month
+SELECT * FROM AirbnbStaging WHERE reviews_per_month <= 0 OR reviews_per_month IS NULL
+
+-- 15795 of these rows have no reviews, so reviews per month should be 0
+-- the remaining 22 rows will have reviews_per_month set to -1 to indicate this value is unknown
+-- cannot calculate this as the amount of time the listing has been posted is not provided
+SELECT * FROM AirbnbStaging 
+WHERE (reviews_per_month <= 0 OR reviews_per_month IS NULL)
+AND num_reviews = 0
+
+UPDATE AirbnbStaging
+SET [reviews_per_month] = 0
+WHERE ([reviews_per_month] <= 0 OR [reviews_per_month] IS NULL)
+AND num_reviews = 0
+
+UPDATE AirbnbStaging
+SET [reviews_per_month] = -1
+WHERE ([reviews_per_month] <= 0 OR [reviews_per_month] IS NULL)
+AND num_reviews > 0
+
+-- review scores are the average rating of a listing on a scale of 1-5
+-- if a listing has reviews but no review_score, this will be filled in with the average
+-- if there are no reviews the score will be kept as NULL instead of being replaced with 0 to not affect the average score
+SELECT DISTINCT review_score FROM AirbnbStaging
+
+-- convert to numeric values
+ALTER TABLE AirbnbStaging
+ALTER COLUMN review_score INT
+
+SELECT AVG(review_score) FROM AirbnbStaging -- avg of 3
+
+UPDATE AirbnbStaging
+SET [review_score] = 3
+WHERE [review_score] IS NULL
+AND [num_reviews] > 0
+
